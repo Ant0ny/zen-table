cb442f36b93564a2030d851eb9ef22ad8915d1f0
Anton Bannikov <bas@umbr.ru>
2021-04-08 17:29:59 +0500

    WIP

--- zen-table.js
+++ zen-table.js
@@ -1,54 +1,52 @@
-(function($) {	
-	$.fn.myClick = function(options) {
+(function($) {
+	$.fn.zenTable = function(options) {
 		let config = $.extend({}, {
 			// single || everywere
-			filter_type: 'single',			
+			filter_type: 'single',
 		}, options);
-		
+
 		function main(e) {
 			let filter_type = e.data('filter_type');
-			
-			if (!filter_type) { 
-				filter_type = config.filter_type 
+
+			if (!filter_type) {
+				filter_type = config.filter_type
 			}
+
+			set_table_filters('.' + e.prop('className'));
 		}
 
-		function set_table_filter(table_class) {
-			if ($(table_class).length) {
-			  var s = '';
+		function set_table_filters(table_class) {
+			var n = $(table_class).length;
+
+			if (n) {
+				$(table_class).each(function(table_index, table_el) {
+			  	$(table_el).data('zen-index', table_index);
+			  });
+
 			  $(table_class).find('thead th').each(function(row_index, row_el) {
-			    s += '<th>'+$(row_el).text()+'</th>';
+			  	$(row_el).html('<span>'+$(row_el).text()+'</span>');
+					$(row_el).append('<input type="text" class="zen-filter zen-filter-'+row_index+'" placeholder="'+$(row_el).text()+'" />');
 			  });
 
-			  var n =$(".table-filters").length;
-			  n += 1;
-
-			  $(table_class).before('<table class="table-filters table-filter-'+n+'"><thead>'+s+'</thead></table>');
-
-			  $(table_class).parent().find('.table-filter-'+n+' thead th').each(function() {
-			    var title = $(this).text();
-			    $(this).html( '<input type="text" placeholder="'+title+'" />' );
-			  });
-
-			  $(document).on('keyup change', '.table-filter-'+n+' thead th input', function() {
-			    var arrs = [];
+			  $(document).on('keyup change', table_class+' thead th', function() {
+			    var row_indexes = [];
 			    var filter_count = 0;
 
-			    $('.table-filter-'+n+' thead tr th').each(function(index0, el0) {
+			    $(this).each(function(index0, el0) {
 			      if ($(el0).find('input').val().length) {
 			        var replace = $(el0).find('input').val();
 			        var replace2 = replace.replace(/[[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
 			        var re = new RegExp(replace2,"gi");
 
-			        arrs.push(is_filter_return_rows(index0, re, table_class));
+			        row_indexes.push(matched_row_indexes(index0, re, table_class));
 			        filter_count++;
 			      }
 			    });
 
-			    var prev_arr = [];
+			    /*var prev_arr = [];
 			    arrs.forEach(function(item, i, arr) {
-					if (i == 0){	
-			        prev_arr = item;	
+					if (i == 0){
+			        prev_arr = item;
 			      }
 			      if (i > 0) {
 			        var common = $.grep(prev_arr, function(element) {
@@ -56,14 +54,14 @@
 			        });
 			        prev_arr = common;
 			      }
-			    });
+			    });*/
 
 			    $(table_class).find('tbody tr').each(function(index2, el) {
-			      if (prev_arr.indexOf(index2) != -1){
+			      /*if (prev_arr.indexOf(index2) != -1){
 			        $(el).show();
 			      } else {
 			        $(el).hide();
-			      }
+			      }*/
 			    });
 
 			    if (!filter_count){
@@ -71,9 +69,21 @@
 			    }
 			  });
 			}
-		}	
+		}
 
-		this.each(function() { main($(this)); });
+	  function matched_row_indexes(index, regexp, table_class) {
+	    var arr = [];
+	    $(table_class).find('tbody tr').each(function(index2, el) {
+	      if ($(el).find('td').eq(index).text().match(regexp)) {
+	        arr.push(index2);
+	      }
+	    });
+	    return arr;
+	  }
+
+		this.each(function() {
+		  main($(this));
+		});
 		return this;
 	};
 })(jQuery);
